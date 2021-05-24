d20a63e1f4228ca42b928878a5837443c072e9e9
YooSijin <yousj9461@gmail.com>
2021-05-24 18:09:47 +0900

    0524

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -2,12 +2,11 @@
   <div id="comment">
     <div v-for="(comment, idx) in comments" :key="idx" :comment="comment">
       <ul>
-        <li>{{ comment.username }}|{{ comment.contents }}
-        <div>
-          <input type="text" v-model.trim="contents" @keyup.enter="updateComment(comment)">
-          <button @click="updateComment(comment)">수정</button>
-          <button @click="deleteComment(comment)">X</button>
-        </div>
+        <li>{{ comment.username }}|{{ comment.contents }}{{comment}}{{idx}}
+          <input v-if="username === comment.username" type="text" @keyup.enter="updateComment(idx)" :value="comment.contents">
+          <button @click="updateComment(idx)">수정하기</button>
+          <button v-if="updatecount>0" @click="watchHistory">수정내역</button>
+          <button @click="deleteComment(idx)">X</button>
         </li>
       </ul>
     </div>
@@ -28,6 +27,8 @@ export default {
     return {
       contents: '',
       username: '',
+      updatecount: 0,
+      history: [],
     }
   },
   methods: {
@@ -63,26 +64,34 @@ export default {
           })
         }
     },
-    updateComment: function (comment) {
+
+    updateComment: function (idx) {
       const review = {
         username: this.$store.state.username,
         contents: this.contents,
       }
       axios({
         method: 'put',
-        url: `http://127.0.0.1:8000/comments/update/${comment.id}/`,
+        url: `http://127.0.0.1:8000/comments/update/${idx}/`,
         data: review,
         headers: this.setToken()
       })
         .then((res) => {
           console.log(res)
+          this.data.updatecount += 1
           
         })
     },
-    deleteComment: function (comment) {
+    deleteComment: function (idx) {
+      const review = {
+        username: this.$store.state.username,
+        contents: this.contents,
+      }
+      // console.log(idx)
       axios({
-        method: 'delete',
-        url: `http://127.0.0.1:8000/comments/delete/${comment.id}/`,
+        method: 'put',
+        url: `http://127.0.0.1:8000/comments/delete/${idx}/`,
+        data: review,
         headers: this.setToken()
       })
         .then((res) => {
---
+++ src/components/CommentHistory.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="commenthistory">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommentHistory',
+  
+}
+</script>
+
+<style>
+
+</style>
